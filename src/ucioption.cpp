b564954bd5203a824203401493fabeae9ef8de3c
stuwph <stuwph@live.de>
2019-12-22 14:50:46 +0100

    local MoveOverhead = 150

--- src/ucioption.cpp
+++ src/ucioption.cpp
@@ -68,7 +68,7 @@ void init(OptionsMap& o) {
   o["Ponder"]                << Option(false);
   o["MultiPV"]               << Option(1, 1, 500);
   o["Skill Level"]           << Option(20, 0, 20);
-  o["Move Overhead"]         << Option(60, 0, 5000);
+  o["Move Overhead"]         << Option(150, 0, 5000);
   o["Minimum Thinking Time"] << Option(20, 0, 5000);
   o["Slow Mover"]            << Option(84, 10, 1000);
   o["nodestime"]             << Option(0, 0, 10000);
